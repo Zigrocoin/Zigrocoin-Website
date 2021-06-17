7c4c155f5b56e710a80b0a946a96c6dc29341ad7
Zigrocoin <85820669+Zigrocoin@users.noreply.github.com>
2021-06-17 16:56:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -9,44 +9,21 @@ const navbarlinks = document.getElementsByClassName('nav_links')[0]
 
 toggleButton.addEventListener('click', () => {
     navbarlinks.classList.toggle('active')
-})
+}) 
 
 const ScrollToFAQ = document.querySelector("#faq");
 const ScrollToWhatis = document.querySelector("#what");
 const ScrollToHowTo = document.querySelector("#howto");
-const ScrollToHowToButton = document.querySelector("#howto_button");
-
-ScrollToFAQ.addEventListener('click', function() {
-    window.scrollTo({
-        top: 3000,
-        left: 0,
-        behavior: "smooth"
-    });
-});
+const ScrollToHowToButton = document.querySelector("#howto_button"); 
 
 ScrollToWhatis.addEventListener('click', function() {
     window.scrollTo({
-        top: 500,
+        top: whatis,
         left: 0,
         behavior: "smooth"
     });
 });
 
-ScrollToHowTo.addEventListener('click', function() {
-    window.scrollTo({
-        top: 900,
-        left: 0,
-        behavior: "smooth"
-    });
-});
-
-ScrollToHowToButton.addEventListener('click', function() {
-    window.scrollTo({
-        top: 900,
-        left: 0,
-        behavior: "smooth"
-    });
-});
 
 
 
